e39612ac59b57809022128933b1602600d183c13
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-04-07 12:52:56 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
 """
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.237.45.220', '54.237.63.152']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """Deploys an archive on the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        archive_name = archive_path.split("/")[-1]
+        min_ext = archive_name.split(".")[0]
+        dest = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(dest, min_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, dest, min_ext))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(dest, min_ext))
+        run('rm -rf {}{}/web_static'.format(dest, min_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(dest, min_ext))
         return True
-    except:
+    except PathNotFound:
         return False
