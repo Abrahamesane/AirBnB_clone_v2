4d0bb3827558229f1d80c9ccc0624c882ccbeaba
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-04-24 17:06:25 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a Flask web application listening on 0.0.0.0, port 5000
+Routes:
+    /states: display a HTML page: (inside the tag BODY)
+        H1 tag: “States”
+        UL tag: with the list of all State objects present
+        in DBStorage sorted by name (A->Z) tip
+            LI tag: description of one State: <state.id>: <B><state.name></B>
+    /states/<id>: display a HTML page: (inside the tag BODY)
+        If a State object is found with this id:
+            H1 tag: “State: ”
+            H3 tag: “Cities:”
+            UL tag: with the list of City objects linked to the State
+            sorted by name (A->Z)
+                LI tag: description of one City: <city.id>: <B><city.name></B>
+    Otherwise:
+        H1 tag: “Not found!”
+"""
 from models import storage
-from models.state import State
-from os import environ
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """ Displays an HTML page with a list of all states """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """ Displays an HTML page with info about <id>, if it exists """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown(exc):
+    """ Remove the current SQLAlchemy session """
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_state(id=""):
-    """ displays a HTML page with a list of cities by states """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    found = 0
-    state = ""
-    cities = []
-
-    for i in states:
-        if id == i.id:
-            state = i
-            found = 1
-            break
-    if found:
-        states = sorted(state.cities, key=lambda k: k.name)
-        state = state.name
-
-    if id and not found:
-        found = 2
-
-    return render_template('9-states.html',
-                           state=state,
-                           array=states,
-                           found=found)
-
-
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
